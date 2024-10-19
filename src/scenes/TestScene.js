c6128e4514c6e0ee37ed99b253e428f94efa6baa
“2556575” <2556575@students.wits.ac.za>
2024-10-19 17:57:14 +0200

    Player mechanics

--- src/scenes/TestScene.js
+++ src/scenes/TestScene.js
@@ -1,60 +1,50 @@
-import Scene from "./Scene.js"
-import EVENTS from './../Events.js';
-import StartScene from "./StartupScene.js"
-import * as THREE from './../../node_modules/three/build/three.module.js';
+import Scene from "./Scene.js";
+import EVENTS from "./../Events.js";
+import StartScene from "./StartupScene.js";
+import * as THREE from "three";
 
 const material = new THREE.MeshBasicMaterial({ color: 0xa0ff0f });
 
 export default class TestScene extends Scene {
-    constructor(camera){
-        super(camera);
+  constructor(camera) {
+    super(camera);
 
-        this.m_MainCamera = camera;
-        this.m_MainCamera.position.z = 7;
-        // Create a cube
-        const geometry = new THREE.BoxGeometry();
-        this.cube = new THREE.Mesh(geometry, material);
-        this.m_Scene.add(this.cube);
-        
+    this.m_MainCamera = camera;
+    this.m_MainCamera.position.z = 7;
+    // Create a cube
+    const geometry = new THREE.BoxGeometry();
+    this.cube = new THREE.Mesh(geometry, material);
+    this.m_Scene.add(this.cube);
+  }
+
+  OnBegin() {}
+
+  OnUpdate(deltaTime) {
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+    this.cube.rotation.z += 0.01;
+    //this.m_MainCamera.position.z += 0.5;
+    //material.color.setHSL(Math.sin(this.m_FrameNumber / 60) * 0.5 + 0.5, 1, 0.5);
+    if (EVENTS.eventHandler.IsKeyHeld(EVENTS.KEY.W)) {
+      console.log("W is held");
     }
 
-
-    OnBegin(){
-
+    if (EVENTS.eventHandler.IsKeyPressed(EVENTS.KEY.A)) {
+      console.log("A is pressed");
     }
 
-    OnUpdate(deltaTime){
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
-        this.cube.rotation.z += 0.01;
-        //this.m_MainCamera.position.z += 0.5;
-        //material.color.setHSL(Math.sin(this.m_FrameNumber / 60) * 0.5 + 0.5, 1, 0.5);
-        if (EVENTS.eventHandler.IsKeyHeld(EVENTS.KEY.W)){
-            console.log("W is held");
-        }
-        
-        if (EVENTS.eventHandler.IsKeyPressed(EVENTS.KEY.A)){
-            console.log("A is pressed");
-        }
-        
-        if (EVENTS.eventHandler.IsKeyPressed(EVENTS.KEY.D)){
-            console.log("D key pressed");
-        }
-
-        if (EVENTS.eventHandler.IsKeyPressed(EVENTS.KEY.ONE)){
-            this.SwitchScenes(new StartScene(this.m_MainCamera));
-        }
+    if (EVENTS.eventHandler.IsKeyPressed(EVENTS.KEY.D)) {
+      console.log("D key pressed");
     }
 
-    OnPreRender(){
-
+    if (EVENTS.eventHandler.IsKeyPressed(EVENTS.KEY.ONE)) {
+      this.SwitchScenes(new StartScene(this.m_MainCamera));
     }
+  }
 
-    OnUIRender(){
+  OnPreRender() {}
 
-    }
+  OnUIRender() {}
 
-    OnEnd(){
-
-    }
+  OnEnd() {}
 }
