5c5067ef2c196e4ee67f1cf2bb33b93ee084fb8b
Here06 <2543080@students.wits.ac.za>
2024-11-02 11:06:34 +0200

    removed the helper for the point light as well increasing luminosity of lightning

--- src/scenes/setup/EnvironmentManager.js
+++ src/scenes/setup/EnvironmentManager.js
@@ -105,14 +105,11 @@ export class EnvironmentManager {
     bulbMesh.position.set(-18.2, 3.0, 8.5);
     this.scene.add(bulbMesh);
     this.scene.add(this.lampLight);
-
-    const lightHelper = new THREE.PointLightHelper(this.lampLight, 0.5);
-    this.scene.add(lightHelper);
 }
 
 animate() {
   if (this.lampLight) {
-      // Set the light off occasionally (e.g., 20% chance per frame)
+      //20% chance per frame)
       const flickerOff = Math.random() < 0.2;
 
       if (flickerOff) {
--- src/scenes/setup/LightningEffect.js
+++ src/scenes/setup/LightningEffect.js
@@ -67,17 +67,21 @@ export class LightningEffect {
         };
 
         // Create multiple lightning strikes
-        for (let i = 0; i < 1; i++) {
+        for (let i = 0; i < 2; i++) {
             const lightningStrike = new LightningStrike(rayParams);
-            const lightningMesh = new THREE.Mesh(lightningStrike, new THREE.MeshBasicMaterial({ color: 0xffffff }));
+            const lightningMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+            lightningMaterial.emissive = new THREE.Color(0xffffff);
+            lightningMaterial.emissiveIntensity = 15; 
+            const lightningMesh = new THREE.Mesh(lightningStrike, lightningMaterial);
+            
             this.lightningMeshes.push({ mesh: lightningMesh, strike: lightningStrike });
             this.scene.add(lightningMesh);
         }
 
         // Create an outline effect for lightning
         const outlinePass = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), this.scene, this.camera, this.lightningMeshes.map(l => l.mesh));
-        outlinePass.edgeStrength = 2;
-        outlinePass.edgeGlow = 2.5;
+        outlinePass.edgeStrength = 3;
+        outlinePass.edgeGlow = 3.5;
         outlinePass.edgeThickness = 1;
         outlinePass.visibleEdgeColor.set(0x00aaff);
         this.composer.addPass(outlinePass);
@@ -111,11 +115,11 @@ export class LightningEffect {
     }
 
     setPosition(position) {
-        const highAbovePosition = position.clone().add(new THREE.Vector3(0, 50, 0)); // Adjust 50 to any height you prefer
+        const highAbovePosition = position.clone().add(new THREE.Vector3(0, 50, 0)); 
     
         this.lightningMeshes.forEach(({ strike }) => {
             strike.rayParameters.sourceOffset.copy(highAbovePosition); // Start point high above
-            strike.rayParameters.destOffset.copy(position); // End point at ground level
+            strike.rayParameters.destOffset.copy(position); // End point
         });
     }
 
